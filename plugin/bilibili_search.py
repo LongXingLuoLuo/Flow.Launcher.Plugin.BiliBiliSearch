a10e782aa1db3c3bb776f22d876b257e975d030e
LXLL <2473475362@qq.com>
2025-09-14 21:07:16 +0800

    v0.0.2 添加了直播间关注功能，显示关注的已开播直播间

--- plugin/bilibili_search.py
+++ plugin/bilibili_search.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from typing import MutableMapping
 
 from requests import get
@@ -9,16 +10,47 @@ proxies: MutableMapping = {"http": None, "https": None}
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
-suggest_url = r"https://api.bilibili.cn/suggest"
-search_url = r"https://search.bilibili.com/all"
+SUGGEST_URL = r"https://api.bilibili.cn/suggest"
+SEARCH_URL = r"https://search.bilibili.com/all"
+DYNAMIC_URL = r"https://t.bilibili.com/"
+ROOM_INFO_URL = r"https://api.live.bilibili.com/room/v1/Room/get_info"
+ROOM_URL = r"https://live.bilibili.com/"
 
+class RoomInfo(object):
+    roomStatus: bool # 有无房间
+    liveStatus: int # 是否开播
+    url: str # 直播间网页 url
+    title: str # 直播间标题
+    cover: str # 直播间封面 url
+    roomId: int # 直播间 id
+    online: int # 直播间人气
+    liveTime: datetime # 开播时间
+
+    def __init__(self):
+        """
+        BiliBili 直播间信息数据结构
+        """
+        self.roomStatus = True
+        self.liveStatus = 0
+        self.url = ""
+        self.title = ""
+        self.cover = ""
+        self.roomId = 0
+        self.online = 0
+        self.liveTime = datetime.now()
+
+    def __str__(self):
+        return f"RoomInfo(roomStatus={self.roomStatus}, liveStatus={self.liveStatus}, url='{self.url}', title='{self.title}', cover='{self.cover}', roomId={self.roomId}, online={self.online}, liveTime={self.liveTime})"
+
+    def __repr__(self):
+        return self.__str__()
 
 def get_query_suggestions(query: str) -> list[str]:
     logger.debug(f"Get query suggestions for {query}")
     if query == "" or query is None:
         logger.debug("Empty query")
         return []
-    resp = get(url=suggest_url, params={"term": query}, proxies=proxies, headers=headers)
+    resp = get(url=SUGGEST_URL, params={"term": query}, proxies=proxies, headers=headers)
     if resp.status_code != 200:
         logger.error(f"Suggest resp: {resp.status_code}")
         return []
@@ -31,9 +63,40 @@ def get_query_suggestions(query: str) -> list[str]:
 
 def get_search_url(query: str) -> str:
     logger.debug(f"Search url: {query}")
-    return f"{search_url}?keyword={query}"
+    return f"{SEARCH_URL}?keyword={query}"
+
+def get_dynamic_url() -> str:
+    logger.debug(f"Get dynamic url: {DYNAMIC_URL}")
+    return DYNAMIC_URL
+
+def get_room_info(room_id: int) -> RoomInfo:
+    logger.debug(f"Get room of mid: {room_id}")
+    info = RoomInfo()
+    resp = get(url=ROOM_INFO_URL, proxies=proxies, headers=headers, params={"room_id": room_id})
+    if resp.status_code != 200:
+        logger.debug(f"Get room info resp: {resp.status_code}")
+        return info
+    if resp.json()["code"] != 0:
+        logger.debug(f"Room is not live: {resp.json()}")
+        info.roomStatus = False
+        return info
+    logger.info(f"Get room info resp: {resp.json()}")
+    data = resp.json()["data"]
+    info.liveStatus = data["live_status"]
+
+    info.title = data["title"]
+    info.cover = data["user_cover"]
+    info.roomId = data["room_id"]
+    info.online = data["online"]
+    info.url = f"{ROOM_URL}{info.roomId}"
+    if data['live_time'] == "0000-00-00 00:00:00":
+        info.liveTime = datetime.now()
+    return info
 
 if __name__ == "__main__":
-    logger.info("Start bilibili search")
+    logger.debug("Start bilibili search")
     logger.debug(get_query_suggestions("奶绿"))
     logger.debug(get_search_url("奶绿"))
+    logger.debug(get_dynamic_url())
+    logger.debug(get_room_info(898170))
+    logger.debug("End bilibili search")
--- plugin/main.py
+++ plugin/main.py
@@ -1,41 +1,127 @@
 # -*- coding: utf-8 -*-
-from typing import MutableMapping
 
-from flox import Flox, ICON_SETTINGS
-from plugin.bilibili_search import get_search_url, get_query_suggestions
 import webbrowser
+from functools import cached_property
+
+from flox import Flox
+
+from plugin.bilibili_search import get_search_url, get_query_suggestions, get_dynamic_url, get_room_info
 from plugin.log import logger
+
+ROOM_ID_LIST_SPILT = ','
 
 
 class BiliBiliSearchPlugin(Flox):
 
+    @cached_property
+    def app_icon(self):
+        return self.plugindir + "/Images/app.png"
+
+    @property
+    def room_id_list(self) -> list[int]:
+        room_id_list = self.settings.get("room_id_list", '')
+        if room_id_list != '':
+            return [int(rid) for rid in room_id_list.split(ROOM_ID_LIST_SPILT) if rid.isdigit()]
+        return []
+
     def query(self, query) -> None:
         logger.debug(f"Suggest query: {query}")
+        if query is None or query == "":
+            self.add_item(
+                title="动态",
+                subtitle="动态页面",
+                icon=self.app_icon,
+                method=self.open_url,
+                parameters=[get_dynamic_url()]
+            )
+            for room_id in self.room_id_list:
+                info = get_room_info(room_id)
+                if not info.roomStatus:
+                    subtitle = "不存在该直播间"
+                    self.add_item(
+                        title=f"直播: {info.title}",
+                        subtitle=subtitle,
+                        icon=self.app_icon,
+                        context=[room_id]
+                    )
+                    return
+                if info.liveStatus != 1:
+                    subtitle = "未开播"
+                else:
+                    subtitle = f"直播中，观看人数: {str(info.online)}，开播时间: {info.liveTime.strftime('%Y-%m-%d %H:%M:%S')}"
+                self.add_item(
+                    title=f"直播: {info.title}",
+                    subtitle=subtitle,
+                    icon=self.app_icon,
+                    method=self.open_url,
+                    parameters=[info.url],
+                    context=[room_id]
+                )
+            return
+        if query.isdigit():
+            rid = int(query)
+            info = get_room_info(rid)
+            if not info.roomStatus or info.liveStatus != 1:
+                subtitle = "未开播"
+            else:
+                subtitle = f"直播中，观看人数: {str(info.online)}，开播时间: {info.liveTime.strftime('%Y-%m-%d %H:%M:%S')}"
+            if info.roomStatus:
+                self.add_item(
+                    title=f"直播: {info.title}",
+                    subtitle=subtitle,
+                    icon=self.app_icon,
+                    method=self.open_url,
+                    parameters=[info.url],
+                    context=[rid]
+                )
         for suggestion in get_query_suggestions(query):
             self.add_item(
                 title=suggestion,
                 subtitle=suggestion,
-                icon=ICON_SETTINGS,
-                method=self.open_url,
+                icon=self.plugindir + "/Images/app.png",
+                method=self.open_search_url,
                 parameters=[suggestion],
-                context=[suggestion]
             )
             logger.info(f"Suggestion add: {suggestion}")
 
     def context_menu(self, data):
-        context = data["context"]
+        rid = data[0]
         self.add_item(
-            title="打开链接",
-            subtitle="Open in BiliBili",
-            method="open_url",
-            parameters=[context]
+            title=f"添加直播间 {rid}",
+            subtitle=f"将该直播间添加到关注列表",
+            method=self.add_room_id,
+            parameters=[rid]
         )
-        logger.info(f"Context menu add: {context}")
-        return self._results
+        self.add_item(
+            title=f"移除直播间 {rid}",
+            subtitle="从关注列表移除该直播间",
+            method=self.remove_room_id,
+            parameters=[rid]
+        )
+        logger.info(f"Context menu add: {data}")
 
-    def open_url(self, query):
-        logger.debug(f"Open url: {query}")
-        webbrowser.open(get_search_url(query))
+    def open_search_url(self, query: str):
+        url = get_search_url(query)
+        logger.debug(f"Open search url: {url}")
+        self.open_url(url)
+
+    def open_url(self, url: str):
+        logger.debug(f"Open url: {url}")
+        webbrowser.open(url)
+
+    def add_room_id(self, room_id: int):
+        room_id_list = self.room_id_list
+        if room_id not in room_id_list:
+            room_id_list.append(room_id)
+            self.settings["room_id_list"] = ROOM_ID_LIST_SPILT.join([str(rid) for rid in room_id_list])
+            logger.debug(f"Room id: {room_id} added to room id: {self.room_id_list}")
+
+    def remove_room_id(self, room_id: int):
+        room_id_list = self.room_id_list
+        if room_id in room_id_list:
+            room_id_list.remove(room_id)
+            self.settings["room_id_list"] = ROOM_ID_LIST_SPILT.join([str(rid) for rid in room_id_list])
+            logger.debug(f"Room id: {room_id} not in room id: {self.room_id_list}")
 
 
 if __name__ == "__main__":
